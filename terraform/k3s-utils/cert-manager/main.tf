3a501aacf65b27a6191c1b6db5c0f5dc25751ad6
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-05-17 20:39:34 -0700

    Add cert manager deployment (#10)
    * Add cert manager deployment
    
    * Fix wrongly entered var
    
    * chore: auto-format Terraform files [skip ci]
    
    * Add badges because why not
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ terraform/k3s-utils/cert-manager/main.tf
@@ -0,0 +1,18 @@
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = var.kube_namespace
+  create_namespace = true
+  version          = var.version
+
+  set {
+    name  = "prometheus.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
---
+++ terraform/k3s-utils/cert-manager/variables.tf
@@ -0,0 +1,17 @@
+variable "kube_config_path" {
+  default     = "~/.kube/config"
+  type        = string
+  description = "Path to kubeconfig file relative to where this script will run"
+}
+
+variable "kube_namespace" {
+  default     = "cert-manager"
+  type        = string
+  description = "Namespace where the cert-manager will be deployed"
+}
+
+variable "version" {
+  default     = "1.17.2"
+  type        = string
+  description = "Cert manager version"
+}

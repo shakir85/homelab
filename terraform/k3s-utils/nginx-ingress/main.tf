70c1ffc60dcb7cdb31a45c9bac4183f890110c36
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-05-20 12:20:22 -0700

    Add TF script fo Nginx ingress deployment

---
+++ terraform/k3s-utils/nginx-ingress/main.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_namespace" "ingress_ns" {
+  metadata {
+    name = var.kube_namespace
+  }
+}
+
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-name"
+  chart      = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  namespace  = kubernetes_namespace.ingress_ns.metadata[0].name
+  values     = [
+    file("../../../k8s/charts/nginx-ingress/values.yml")
+    ]
+}

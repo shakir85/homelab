e4ec4f344419a429501ba1e7f97c6609074d4627
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-05-22 17:49:33 -0700

    Add k8s resources and update doc notes (#26)

--- terraform/k3s-utils/metallb-crd/main.tf
+++ terraform/k3s-utils/metallb-crd/main.tf
@@ -1,3 +1,20 @@
+/**
+ * ## MetalLB Custom Resource Definitions (CRDs) Deployment
+ *
+ * This module installs the official MetalLB CRDs for Layer 2 advertisement with a static IP address pool using Terraform.
+ *
+ * ## Requirements
+ * Set IP address ranges for MetalLB to allocate when a LoadBalancer Service is deployed;
+ * ensure the range is available in your router - e.g., 192.168.50.90-192.168.50.99 may be reserved
+ * for static mapping.
+ *
+ *
+ * This will:
+ * - Deploy a `IPAddressPool` CRD
+ * - Deploy a `L2Advertisement` CRD that references the IP pool
+ *
+ * > Note: MetalLB must be up and running **before** running this configuration.
+ */
 resource "kubernetes_manifest" "ip_address_pool" {
   manifest = {
     apiVersion = "metallb.io/v1beta1"
--- terraform/modules/metallb/main.tf
+++ terraform/modules/metallb/main.tf
@@ -2,33 +2,19 @@
  * ## MetalLB Helm Deployment
  *
  * This module installs the official [MetalLB Helm chart](https://metallb.universe.tf/installation/#installation-with-helm)
- * and configures a simple Layer 2 advertisement with a static IP address pool using Terraform.
- *
- * ## Requirements
- * Set IP address ranges for MetalLB to allocate from when a LoadBalancer Service is deployed;
- * ensure the range is available in your router - e.g., 192.168.50.90-192.168.50.99 may be reserved
- * for static mapping.
  *
  * ## Usage
  *
  * ```hcl
  * module "metallb" {
  *   source  = "path/to/this/module"
- *   # Required variables
- *   metallb_chart_version = "0.14.5"
- *   ip_range              = ["10.10.50.90-10.10.50.99"]
- *   pool_name             = "first-pool"
- *   advertisement_name    = "main-advertisement"
  * }
  * ```
  *
  * This will:
- * - Install the MetalLB Helm chart in the `metallb-system` namespace
- * - Deploy a `IPAddressPool` CRD
- * - Deploy a `L2Advertisement` CRD that references the IP pool
+ * - Install the MetalLB Helm chart version specified in `var.metallb_version` in the `metallb-system` namespace
  *
- * > Note: MetalLB CRDs must be created **after** the Helm chart installation, so this configuration
- * includes appropriate `depends_on` logic to ensure ordering.
+ * > Note: If you plan to install MetalLB Custom Resource Definition (CRDs) for IP advertisement, they must be created **after** the Helm chart installation.
  */
 terraform {
   required_version = ">= 1.5.7"

8f87bf24aaf598ebf3fffaba20093b7bdac70ffd
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-05-25 13:33:04 -0700

    NFS-Based Dynamic PVC Provisioning and Helmfile Deployment Structure (#33)
    * Add csi module
    
    * Add storage class
    
    * Initial pvc work
    
    * Some updates
    
    * Decouple storage lifecycle
    
    * Inisital helmfile templates

---
+++ terraform/k3s-main/storage-class/main.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_storage_class" "nfs" {
+  metadata {
+    name = "nfs-client"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+
+  storage_provisioner = "nfs.csi.k8s.io"
+
+  parameters = {
+    server = "10.10.50.38"
+    share  = "/volume1/k3s-main"
+  }
+
+  reclaim_policy         = "Retain"
+  volume_binding_mode    = "Immediate"
+  allow_volume_expansion = true
+
+  mount_options = [
+    "nfsvers=4.1",
+    "hard",
+    "timeo=600",
+    "retrans=5"
+  ]
+}

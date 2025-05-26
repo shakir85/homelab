17b352686d8f75efcff83d0583a628d9bf3f0996
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-05-25 21:09:53 -0700

    Fix Helm failure due to improperly binded roles

--- terraform/modules/gha-rbac/main.tf
+++ terraform/modules/gha-rbac/main.tf
@@ -22,6 +22,16 @@
  * This creates:
  * - A Role in the `target_namespace` with permissions to manage the type of k8s resources defined in the `kubernetes_role` block.
  * - A RoleBinding to bind the remote service account (`gha-runner` in `cicd`) to that `kubernetes_role` role.
+ *
+ * ## Least Privilege Implementation
+ *
+ * This module is designed with the principle of leat privilege in mind. And it's mainly to allow
+ * Helm to install, upgrade, and uninstall charts. Charts in this repo deploy only services,
+ * ingress, pvc, and deployment, and Helm typically needs RBAC to:
+ *
+ *   - get, create, update, patch, delete — for managing resources like Deployments, Services, PVCs, etc.
+ *   - list — for internal release tracking (e.g. existing resources or Helm Secrets).
+ *   - watch — not required for Helm CLI itself, but I will keep it for controllers.
  */
 
 terraform {
@@ -42,35 +52,38 @@ resource "kubernetes_role" "gha_target_ns_role" {
     labels    = var.shared_labels
   }
 
+  # Core API resources - pods, services, configmaps
   rule {
     api_groups = [""]
-    resources  = ["configmaps", "services", "pods"]
-    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+    resources  = ["pods", "services", "configmaps", "persistentvolumeclaims"]
+    verbs      = ["get", "list", "create", "update", "patch", "delete"]
   }
 
+  # Helm itself interacts with K8s Secrets to manage its internal release state.
+  # It can be configured to store its state in ConfigMaps instead via `--storage configmaps`
+  # But I don't want to change helm's default functionality / settings.
+  # Maybe a good TODO task candidate?!
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["get", "list", "create", "update", "patch", "delete"]
+
+  }
+
+  # Deployments and ReplicaSets
   rule {
     api_groups = ["apps"]
     resources  = ["deployments", "replicasets"]
-    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+    verbs      = ["get", "list", "create", "update", "patch", "delete"]
   }
 
-  #   rule {
-  #     api_groups = ["networking.k8s.io"]
-  #     resources  = ["networkpolicies"]
-  #     verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
-  #   }
+  # For Ingress resources
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses"]
+    verbs      = ["get", "list", "create", "update", "patch", "delete"]
+  }
 
-  #   rule {
-  #     api_groups = ["policy"]
-  #     resources  = ["poddisruptionbudgets"]
-  #     verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
-  #   }
-
-  #   rule {
-  #     api_groups = [""]
-  #     resources  = ["serviceaccounts"]
-  #     verbs      = ["get"]
-  #   }
 }
 
 resource "kubernetes_role_binding" "gha_target_ns_binding" {

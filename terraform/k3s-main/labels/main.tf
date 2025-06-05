32d649636193639c0cf4ccbb30e83a5eb22f3277
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-06-04 20:54:39 -0700

    More nodes (#70)
    * Add nodes
    
    * Organize labels, apps and nodes
    
    * Fix provider lint constrain
    
    * chore: update Terraform docs [skip ci]
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/k3s-main/labels/main.tf
+++ terraform/k3s-main/labels/main.tf
@@ -1,18 +1,61 @@
-module "k3s-main-labels" {
+module "k3s-main-size-labels" {
   source = "../../modules/node-labels"
 
   node_label_map = {
     "k3s-main-node-1" = {
-      "size" = "small"
+      (var.size_label_key) = "small"
     }
     "k3s-main-node-2" = {
-      "size" = "medium"
+      (var.size_label_key) = "medium"
     }
     "k3s-main-node-3" = {
-      "size" = "medium"
+      (var.size_label_key) = "medium"
     }
     "k3s-main-node-4" = {
-      "size" = "large"
+      (var.size_label_key) = "large"
+    }
+    "k3s-main-nuc-1" = {
+      (var.size_label_key) = "small"
+    }
+    "k3s-main-nuc-2" = {
+      (var.size_label_key) = "small"
+    }
+    "k3s-main-nuc-3" = {
+      (var.size_label_key) = "x-large"
+    }
+    "k3s-main-tc-1" = {
+      (var.size_label_key) = "x-large"
+    }
+  }
+}
+
+module "k3s-main-name-labels" {
+  source = "../../modules/node-labels"
+
+  node_label_map = {
+    "k3s-main-node-1" = {
+      (var.name_label_key) = "k3s-main-node-1"
+    }
+    "k3s-main-node-2" = {
+      (var.name_label_key) = "k3s-main-node-2"
+    }
+    "k3s-main-node-3" = {
+      (var.name_label_key) = "k3s-main-node-3"
+    }
+    "k3s-main-node-4" = {
+      (var.name_label_key) = "k3s-main-node-4"
+    }
+    "k3s-main-nuc-1" = {
+      (var.name_label_key) = "k3s-main-nuc-1"
+    }
+    "k3s-main-nuc-2" = {
+      (var.name_label_key) = "k3s-main-nuc-2"
+    }
+    "k3s-main-nuc-3" = {
+      (var.name_label_key) = "k3s-main-nuc-3"
+    }
+    "k3s-main-tc-1" = {
+      (var.name_label_key) = "k3s-main-tc-1"
     }
   }
 }
--- terraform/modules/node-labels/main.tf
+++ terraform/modules/node-labels/main.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.5.7"
 
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.36.0"
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
     }
   }
 }

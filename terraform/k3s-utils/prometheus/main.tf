ce6d1c92f765e3d3ab1ad7be4bf7a838d44d604c
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-06-06 15:09:52 -0700

    Initial monitoring stack code

---
+++ terraform/k3s-utils/prometheus/main.tf
@@ -0,0 +1,40 @@
+module "monitoring_namespace" {
+  source = "../../modules/namespace"
+  name   = var.kube_namespace
+  labels = {
+    "app.kubernetes.io/managed-by" = "terraform"
+  }
+}
+
+resource "kubernetes_secret" "grafana_admin_credentials" {
+  metadata {
+    name      = "grafana-admin-credentials"
+    namespace = "monitoring"
+  }
+
+  data = {
+    admin-user     = filebase64("./admin")
+    admin-password = filebase64("./admin-login")
+  }
+
+  type = "Opaque"
+
+  depends_on = [ module.monitoring_namespace ]
+}
+
+resource "helm_release" "prometheus_stack" {
+  name       = "prometheus"
+  namespace  = "monitoring"
+  chart      = "../../../helm/charts/kube-prometheus-stack"
+  version    = "73.2.0"
+  values     = [
+    file("${path.module}/values.yaml")
+  ]
+
+  dependency_update = true
+
+  depends_on = [
+    module.monitoring_namespace,
+    kubernetes_secret.grafana_admin_credentials
+  ]
+}

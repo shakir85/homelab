cf2205e88392bb54d5af4f660c5351f13a6c6b95
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-06-06 22:19:14 -0700

    Use the unused var :p

--- terraform/k3s-utils/prometheus/main.tf
+++ terraform/k3s-utils/prometheus/main.tf
@@ -1,7 +1,7 @@
 # Requires a k8s secret for Grafana admin credentials. See the values.yaml file for more details.
 # The secret should be created using kubectl or Terraform before applying this Helm release.
 resource "helm_release" "prometheus_stack" {
-  name       = "prometheus"
+  name       = var.kube_namespace
   namespace  = "monitoring"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"

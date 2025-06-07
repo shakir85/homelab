7706bd37e4cee7a5f28d4b46551662705a3ef07d
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-06-07 11:20:14 -0700

    Monitoring refactoring (#76)
    * Move tc-1 node to k3s-utils
    
    * Add monitoring stuff

--- terraform/k3s-utils/labels_taints/main.tf
+++ terraform/k3s-utils/labels_taints/main.tf
@@ -11,6 +11,9 @@ module "k3s-utils-size-labels" {
     "k3s-utils-node-3" = {
       (var.size_label_key) = "medium"
     }
+    "k3s-utils-tc-1" = {
+      (var.size_label_key) = "x-large"
+    }
   }
 }
 
@@ -27,5 +30,20 @@ module "k3s-utils-name-labels" {
     "k3s-utils-node-3" = {
       (var.name_label_key) = "k3s-utils-node-3"
     }
+    "k3s-utils-tc-1" = {
+      (var.name_label_key) = "k3s-utils-tc-1"
+    }
+  }
+}
+
+resource "null_resource" "taint_node_k3s_utils_tc_1" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl taint node k3s-utils-tc-1 dedicated=monitoring:NoSchedule --overwrite
+    EOT
+  }
+
+  triggers = {
+    always_run = timestamp()
   }
 }

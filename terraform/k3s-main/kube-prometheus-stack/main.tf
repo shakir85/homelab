e84dfcbebae57ae27939050800d3b230020dee0e
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-07-05 20:38:49 -0700

    Add syslog-promtail (#118)
    * Update promtail to ingest syslog
    
    * Remove UDP port from promtail serivce object
    
    * chore: auto-format Terraform files [skip ci]

--- terraform/k3s-main/kube-prometheus-stack/main.tf
+++ terraform/k3s-main/kube-prometheus-stack/main.tf
@@ -26,3 +26,35 @@ resource "helm_release" "loki_stack" {
   dependency_update = true
   depends_on        = [helm_release.prometheus_stack]
 }
+
+# Expose Promtail Syslog Receiver as a LoadBalancer service
+resource "kubernetes_service" "promtail_syslog" {
+  metadata {
+    name      = "promtail-syslog"
+    namespace = "monitoring"
+
+    labels = {
+      app       = "promtail"
+      component = "syslog-receiver"
+    }
+
+    annotations = {
+      "metallb.universe.tf/address-pool" = "default-pool"
+    }
+  }
+
+  spec {
+    selector = {
+      syslog-receiver = "true"
+    }
+    # MetalLb should assign a stable IP from the default-pool
+    type = "LoadBalancer"
+
+    port {
+      name        = "syslog"
+      port        = 1514
+      target_port = 1514
+      protocol    = "TCP"
+    }
+  }
+}

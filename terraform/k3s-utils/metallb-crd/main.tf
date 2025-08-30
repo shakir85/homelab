20fca977dc6705f29b38e43ddda6a5e537b76e88
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-08-30 10:16:49 -0700

    Refactor metallb-crd on k3s-utils

--- terraform/k3s-utils/metallb-crd/main.tf
+++ terraform/k3s-utils/metallb-crd/main.tf
@@ -1,36 +1,6 @@
-resource "kubernetes_manifest" "ip_address_pool" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "IPAddressPool"
-    metadata = {
-      name      = var.ipv4_address_pool_name
-      namespace = var.kube_namespace
-      labels    = var.shared_labels
-    }
-    spec = {
-      addresses = var.ipv4_address_pool
-    }
-  }
-}
-
-resource "kubernetes_manifest" "l2_advertisement" {
-  manifest = {
-    apiVersion = "metallb.io/v1beta1"
-    kind       = "L2Advertisement"
-    metadata = {
-      name      = "main-advertisement"
-      namespace = var.kube_namespace
-      labels    = var.shared_labels
-    }
-    spec = {
-      ipAddressPools = [var.ipv4_address_pool_name]
-    }
-  }
-
-  depends_on = [kubernetes_manifest.ip_address_pool]
-}
-
-output "kube_context_in_use" {
-  value       = var.kube_context
-  description = "The kubeconfig context being used by the Kubernetes provider"
+module "metallb_crd" {
+  source                 = "../../modules/metallb-crd"
+  ipv4_address_pool_name = "default-pool"
+  ipv4_address_pool      = ["10.10.50.95-10.10.50.99"]
+  metallb_namespace      = "metallb-system"
 }
--- terraform/k3s-utils/metallb-crd/variables.tf
+++ terraform/k3s-utils/metallb-crd/variables.tf
@@ -1,16 +1,3 @@
-
-variable "ipv4_address_pool_name" {
-  type        = string
-  default     = "default-pool"
-  description = "IP addresses pool name. This name will be used in the `L2Advertisement.spec.IPAddressPool` K8s CRD. This is important because if omitted, MetalLB will advertise from any available IP pool in the namespace."
-}
-
-variable "ipv4_address_pool" {
-  type        = list(string)
-  default     = ["10.10.50.95-10.10.50.99"]
-  description = "To generate an address pool. An *address pool* is one or more IPv4 range. This variable is a list of strings for the IPv4 *ranges* that MetalLB will use when provisioning a Service of type LoadBalance. Example values: `['192.168.50.90-192.168.50.99', '192.168.20.45-192.168.20.60']`"
-}
-
 variable "kube_config_path" {
   default     = "~/.kube/config"
   type        = string
@@ -20,19 +7,5 @@ variable "kube_config_path" {
 variable "kube_context" {
   description = "The name of the kubeconfig context to use"
   type        = string
-  default     = "k3s-utils-ctx"
-}
-
-variable "kube_namespace" {
-  type        = string
-  default     = "metallb-system"
-  description = "Namespace where the gha-runner service account will reside (typically 'cicd')"
-}
-
-variable "shared_labels" {
-  description = "Shared labels"
-  type        = map(string)
-  default = {
-    "app.kubernetes.io/managed-by" = "terraform"
-  }
+  default     = "k3s-main-ctx"
 }

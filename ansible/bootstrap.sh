9df4d4da8d2f404262363e69f5f74b555b503cae
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-07 12:27:01 -0700

    Patching Ansible (#161)
    * Remove TF var
    
    * Add dev-cluster playbook
    
    * Fix bootstrap playbook

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -e
 
-#USER=$1
-#PUB_KEY_FILE_PATH=$2
+ANSIBLE_KEY=$1
+BOOTSTRAP_USER=$2
+PUB_KEY_FILE_PATH=$3
 
-echo "Setting remote_user..."
+echo "Setting remote user..."
 
-if [ "$USER" == "" ]; then
-    echo "You didn't provide a remote_user, specify a username that exists on the remote host"
+if [ "$BOOTSTRAP_USER" == "" ]; then
+    echo "You didn't provide a remote user, specify a username that has access to the remote host. Required for Ansible flag: -u"
     exit 1
 fi
 
 if [ "$PUB_KEY_FILE_PATH" == "" ]; then
     echo "Pass the absolute path to id_rsa.pub"
     exit 1
+
+if [ "$ANSIBLE_KEY" == "" ]; then
+    echo "Pass the absolute path to id_rsa"
+    exit 1
+
 else
-    echo -e "Bootstrapping using: \nremote_user = $USER\npublic key = $PUB_KEY_FILE_PATH"
+    echo -e "Bootstrapping using: \nremote_user = $BOOTSTRAP_USER\npublic key = $PUB_KEY_FILE_PATH"
     sleep 3
     echo ""
-    ansible-playbook bootstrap.playbook.yml --key-file="$ans" -u "$USER" --extra-vars="pub_ssh_key_file=$PUB_KEY_FILE_PATH"
+    ansible-playbook bootstrap.playbook.yml --key-file="$ANSIBLE_KEY" -u "$BOOTSTRAP_USER" --extra-vars="pub_ssh_key_file=$PUB_KEY_FILE_PATH"
 fi

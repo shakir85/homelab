1ef70907c99dd4ad8ff05e2d05b20a868937fec7
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-07 19:59:23 -0700

    Update modules version

--- terraform/root-modules/platform/main.tf
+++ terraform/root-modules/platform/main.tf
@@ -1,5 +1,5 @@
-module "metallb-crd" {
-  source                 = "git::https://github.com/shakir85/tf-modules.git//metallb-crd?ref=v0.3.2"
+module "metallb-cr" {
+  source                 = "git::https://github.com/shakir85/tf-modules.git//metallb-cr?ref=v0.3.3"
   ipv4_address_pool_name = var.ipv4_address_pool_name
   ipv4_address_pools     = var.ipv4_address_pools
   kube_namespace         = var.metallb_namespace
@@ -7,12 +7,12 @@ module "metallb-crd" {
 }
 
 module "nginx-ingress" {
-  source         = "git::https://github.com/shakir85/tf-modules.git//nginx-ingress?ref=patch-platform"
+  source         = "git::https://github.com/shakir85/tf-modules.git//nginx-ingress?ref=v0.3.3"
   kube_namespace = var.nginx_namespace
 
-  depends_on = [module.metallb-crd]
+  depends_on = [module.metallb-cr]
 }
 
 module "csi-driver-nfs" {
-  source = "git::https://github.com/shakir85/tf-modules.git//csi-driver-nfs?ref=v0.3.2"
+  source = "git::https://github.com/shakir85/tf-modules.git//csi-driver-nfs?ref=v0.3.3"
 }

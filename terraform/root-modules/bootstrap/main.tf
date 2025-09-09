d540d7b39bf753f009d3f135996865d66b81b734
Shakir Almasaari <35549045+shakir85@users.noreply.github.com>
2025-09-08 17:06:03 -0700

    Bump bootstrap module versions

--- terraform/root-modules/bootstrap/main.tf
+++ terraform/root-modules/bootstrap/main.tf
@@ -1,15 +1,15 @@
 module "metallb" {
-  source           = "git::https://github.com/shakir85/tf-modules.git//metallb?ref=v0.3.2"
+  source           = "git::https://github.com/shakir85/tf-modules.git//metallb?ref=v0.3.3"
   kube_namespace   = var.metallb_namespace
   create_namespace = true
 }
 
 module "cert-manager" {
-  source = "git::https://github.com/shakir85/tf-modules.git//cert-manager?ref=v0.3.2"
+  source = "git::https://github.com/shakir85/tf-modules.git//cert-manager?ref=v0.3.3"
 }
 
 module "gha-arc" {
-  source                     = "git::https://github.com/shakir85/tf-modules.git//gha-arc?ref=v0.3.2"
+  source                     = "git::https://github.com/shakir85/tf-modules.git//gha-arc?ref=v0.3.3"
   github_app_id              = var.github_app_id
   github_app_installation_id = var.github_app_installation_id
   github_app_private_key     = var.github_app_private_key
@@ -18,7 +18,7 @@ module "gha-arc" {
 }
 
 module "runner-deployment" {
-  source           = "git::https://github.com/shakir85/tf-modules.git//gha-runner?ref=v0.3.2"
+  source           = "git::https://github.com/shakir85/tf-modules.git//gha-runner?ref=v0.3.3"
   kube_namespace   = "runners"
   create_namespace = true
   runner_name      = var.name
@@ -29,7 +29,7 @@ module "runner-deployment" {
 }
 
 module "gha-roles" {
-  source                           = "git::https://github.com/shakir85/tf-modules.git//gha-roles?ref=v0.3.2"
+  source                           = "git::https://github.com/shakir85/tf-modules.git//gha-roles?ref=v0.3.3"
   role_name                        = "${module.runner-deployment.runner_name}-role"
   runner_service_account_name      = "${module.runner-deployment.runner_name}-sa"
   runner_service_account_namespace = module.runner-deployment.kube_namespace
